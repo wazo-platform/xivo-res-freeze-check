f8af186ca94b0764f57363310a510b89ac45cd65
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2014-07-09 11:06:32 -0400

    add the CheckFreeze manager command
    The command still does nothing

--- res_freeze_check/res_freeze_check.c
+++ res_freeze_check/res_freeze_check.c
@@ -1,13 +1,31 @@
 #include <asterisk.h>
 #include <asterisk/module.h>
+#include <asterisk/manager.h>
+
+static int check_freeze_action(struct mansession *s, const struct message *m)
+{
+	return AMI_SUCCESS;
+}
 
 static int load(void)
 {
+	ast_manager_register2(
+		"CheckFreeze",
+		EVENT_FLAG_SYSTEM,
+		check_freeze_action,
+		ast_module_info->self,
+		"Check for freezes",
+		"This action may be used to detect some freezes.\n"
+		"This check is not in any way garanteed to succeed in detecting every freezes."
+	);
+
 	return AST_MODULE_LOAD_SUCCESS;
 }
 
 static int unload(void)
 {
+	ast_manager_unregister("CheckFreeze");
+
 	return 0;
 }
 
