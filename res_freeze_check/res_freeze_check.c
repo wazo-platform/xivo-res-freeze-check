4ad40dff77bf25b09ee706c350c21942c9876ec6
Etienne Lessard <elessard@avencall.com>
2014-07-10 12:25:59 -0400

    add some boilerplate for CLI commands
    Add an enable/disable command.

--- res_freeze_check/res_freeze_check.c
+++ res_freeze_check/res_freeze_check.c
@@ -1,7 +1,10 @@
 #include <asterisk.h>
 #include <asterisk/channel.h>
+#include <asterisk/cli.h>
 #include <asterisk/module.h>
 #include <asterisk/manager.h>
+
+static int dangerous_commands_enabled = 0;
 
 static int check_freeze_action(struct mansession *s, const struct message *m)
 {
@@ -28,6 +31,38 @@ static int check_freeze_action(struct mansession *s, const struct message *m)
 	return AMI_SUCCESS;
 }
 
+static char *cli_enable(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a)
+{
+	const char *what;
+
+	switch (cmd) {
+		case CLI_INIT:
+			e->command = "freeze {enable|disable}";
+			e->usage = "Usage: freeze {enable|disable}\n";
+			return NULL;
+		case CLI_GENERATE:
+			return NULL;
+	}
+
+	what = a->argv[e->args - 1];
+
+	if (!strcasecmp(what, "enable")) {
+		dangerous_commands_enabled = 1;
+		ast_cli(a->fd, "Dangerous freeze CLI commands enabled.\n");
+	} else if (!strcasecmp(what, "disable")) {
+		dangerous_commands_enabled = 0;
+		ast_cli(a->fd, "Dangerous freeze CLI commands disabled.\n");
+	} else {
+		return CLI_SHOWUSAGE;
+	}
+
+	return CLI_SUCCESS;
+}
+
+static struct ast_cli_entry cli_entries[] = {
+	AST_CLI_DEFINE(cli_enable, "Enable/Disable dangerous freeze CLI commands"),
+};
+
 static int load_module(void)
 {
 	ast_manager_register2(
@@ -40,11 +75,15 @@ static int load_module(void)
 		"This check is not in any way garanteed to succeed in detecting every freezes."
 	);
 
+	ast_cli_register_multiple(cli_entries, ARRAY_LEN(cli_entries));
+
 	return AST_MODULE_LOAD_SUCCESS;
 }
 
 static int unload_module(void)
 {
+	ast_cli_unregister_multiple(cli_entries, ARRAY_LEN(cli_entries));
+
 	ast_manager_unregister("CheckFreeze");
 
 	return 0;
